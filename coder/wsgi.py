122c1a355116d794fe8f3ec57019c68267db8aff
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2025-11-10 23:19:44 -0300

    ultimo

--- coder/wsgi.py
+++ coder/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # 👈 Importamos WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coder.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)  # 👈 Habilitamos Whitenoise
